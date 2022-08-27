41566266ffd5da1a6a58e7bb519b658a3415a9bf
btsutskiridze <bakatsutskiridze@redberry.ge>
2022-08-27 14:30:56 +0400

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Movie quotes</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <h1 class="bg-purple-300 text-white text-center text-4xl">Movie quotes</h1>
+</body>
+</html>
