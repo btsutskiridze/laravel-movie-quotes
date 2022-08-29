9d3eb40803872b153d6d401907c546817112eef2
btsutskiridze <bakatsutskiridze@redberry.ge>
2022-08-29 22:04:48 +0400

    feat: dropdown menu component

---
+++ resources/views/components/dropdown-item.blade.php
@@ -0,0 +1,15 @@
+@props(['active'=>false])
+
+@php
+    $classes ="block text-left px-3 py-1 
+    text-[1.5rem] rounded-xl leading-6 
+    text-white hover:bg-stone-500 focus:bg-stone-600 
+    hover:text-white focus:text-white";
+    
+    if($active) $classes .= " bg-stone-500 text-white";
+    
+@endphp
+
+<a  {{$attributes(['class'=>$classes])}}>
+    {{$slot}}
+</a>
