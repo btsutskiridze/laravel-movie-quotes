b012038702facf2f8ce1c9e5bb68b7ea1bd59b0c
btsutskiridze <bakatsutskiridze@redberry.ge>
2022-08-30 17:17:49 +0400

    refactor: to make current language button active

--- resources/views/components/lang-aside.blade.php
+++ resources/views/components/lang-aside.blade.php
@@ -1,20 +1,26 @@
 <aside class="fixed ml-4 top-[40%]">
-    <a href="{{route('change.language','en')}}" id="ka" class="border-solid border-2 w-14 h-14 text-2xl grid place-items-center mb-2 rounded-full text-white">en</a>   
-    <a href="{{route('change.language','ka')}}" id="en" class="border-solid border-2 w-14 h-14 text-2xl grid place-items-center mb-2 rounded-full text-white">Ka</a>   
-
-    @if (App::currentLocale()=='en')
-        <script>
-            document.getElementById('ka').classList.add('bg-white','text-[#171717]');
-            document.getElementById('ka').classList.remove('text-white');
-        </script>
-    @else
-        <script>
-            document.getElementById('en').classList.add('bg-white','text-[#171717]');
-            document.getElementById('en').classList.remove('text-white');
-        </script>
-    @endif
-
-
+    <a 
+        href="{{route('change.language','en')}}" 
+        id="ka" 
+        class="
+            border-solid border-2 w-14 h-14 
+            text-2xl grid place-items-center 
+            mb-2 rounded-full text-white
+            {{App::currentLocale()=='en' ? 'bg-white text-[#171717]' : 'text-white' }}
+        ">
+        en
+    </a>   
+    <a 
+        href="{{route('change.language','ka')}}" 
+        id="en" 
+        class="border-solid border-2 
+        w-14 h-14 text-2xl grid 
+        place-items-center mb-2
+        rounded-full text-white
+        {{App::currentLocale()=='ka' ? 'bg-white text-[#171717]' : 'text-white' }}
+        ">
+        Ka
+    </a>  
 </aside>
 {{-- bg-white text-[#171717] --}}
 
