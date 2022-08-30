0fa0f28a26875c8ff9d4e0f3dfa667695e86c62f
btsutskiridze <bakatsutskiridze@redberry.ge>
2022-08-30 12:58:02 +0400

    feat: check active language to give its button style

--- resources/views/components/lang-aside.blade.php
+++ resources/views/components/lang-aside.blade.php
@@ -1,4 +1,21 @@
 <aside class="fixed ml-4 top-[40%]">
-    <a href="en" class=" border-white border-solid border-2 w-14 h-14 text-2xl grid place-items-center mb-2 rounded-full text-white">en</a>   
-    <a href="ka" class="bg-white hover:bg-slate-200 w-14 h-14 text-2xl grid place-items-center rounded-full text-[#171717]">Ka</a>   
+    <a href="en" id="ka" class="border-solid border-2 w-14 h-14 text-2xl grid place-items-center mb-2 rounded-full text-white">en</a>   
+    <a href="ka" id="en" class="border-solid border-2 w-14 h-14 text-2xl grid place-items-center mb-2 rounded-full text-white">Ka</a>   
+
+    @if (App::currentLocale()=='en')
+        <script>
+            document.getElementById('ka').classList.add('bg-white','text-[#171717]');
+            document.getElementById('ka').classList.remove('text-white');
+        </script>
+    @else
+        <script>
+            document.getElementById('en').classList.add('bg-white','text-[#171717]');
+            document.getElementById('en').classList.remove('text-white');
+        </script>
+    @endif
+
+
 </aside>
+{{-- bg-white text-[#171717] --}}
+
+{{-- border-white  hover:bg-slate-200 w-14 h-14 text-2xl grid place-items-center rounded-full  --}}
