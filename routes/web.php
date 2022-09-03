abe63c4814258d7a044032de7060eed84d3c6a2d
btsutskiridze <bakatsutskiridze@redberry.ge>
2022-09-03 19:55:04 +0400

    refactor: group routes

--- routes/web.php
+++ routes/web.php
@@ -17,26 +17,34 @@ use App\Http\Controllers\admin\QuoteController as AdminQuoteController;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::controller(QuoteController::class)->group(function () {
+	Route::get('/', 'index')->name('random.quote');
+	Route::get('movie/{movie:slug}/quotes', 'show')->name('movie.show_quotes');
+});
 
-Route::get('/', [QuoteController::class, 'index'])->name('random.quote');
-Route::get('movie/{movie:slug}/quotes', [QuoteController::class, 'show'])->name('movie.show_quotes');
-
-Route::get('login', [AuthController::class, 'create'])->middleware('guest')->name('login.create');
-Route::post('login', [AuthController::class, 'login'])->middleware('guest')->name('login');
-Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+Route::controller(AuthController::class)->group(function () {
+	Route::get('login', 'create')->middleware('guest')->name('login.create');
+	Route::post('login', 'login')->middleware('guest')->name('login');
+	Route::post('logout', 'logout')->name('logout');
+});
 
 Route::get('language/{locale}', [LanguageController::class, 'change'])->name('language.change');
 
-Route::get('admin/quotes/create', [AdminQuoteController::class, 'create'])->middleware('admin')->name('quotes.create');
-Route::get('admin/quotes', [AdminQuoteController::class, 'show'])->middleware('admin')->name('quotes.show');
-Route::post('admin/quote', [AdminQuoteController::class, 'store'])->middleware('admin')->name('quote.store');
-Route::get('admin/quotes/{quote}/edit', [AdminQuoteController::class, 'edit'])->middleware('admin')->name('quote.edit');
-Route::patch('admin/quotes/{quote}', [AdminQuoteController::class, 'update'])->middleware('admin')->name('quote.update');
-Route::delete('admin/quotes/{quote}', [AdminQuoteController::class, 'destroy'])->middleware('admin')->name('quote.destroy');
-
-Route::get('admin/movies/create', [AdminMovieController::class, 'create'])->middleware('admin')->name('movies.create');
-Route::get('admin/movies', [AdminMovieController::class, 'show'])->middleware('admin')->name('movies.show');
-Route::post('admin/movie', [AdminMovieController::class, 'store'])->middleware('admin')->name('movie.store');
-Route::get('admin/movies/{movie:slug}/edit', [AdminMovieController::class, 'edit'])->middleware('admin')->name('movie.edit');
-Route::patch('admin/movies/{movie:slug}', [AdminMovieController::class, 'update'])->middleware('admin')->name('movie.update');
-Route::delete('admin/movies/{movie:slug}', [AdminMovieController::class, 'destroy'])->middleware('admin')->name('movie.destroy');
+Route::middleware('admin')->group(function () {
+	Route::controller(AdminQuoteController::class)->group(function () {
+		Route::get('admin/quotes/create', 'create')->name('quotes.create');
+		Route::get('admin/quotes', 'show')->name('quotes.show');
+		Route::post('admin/quote', 'store')->name('quote.store');
+		Route::get('admin/quotes/{quote}/edit', 'edit')->name('quote.edit');
+		Route::patch('admin/quotes/{quote}', 'update')->name('quote.update');
+		Route::delete('admin/quotes/{quote}', 'destroy')->name('quote.destroy');
+	});
+	Route::controller(AdminMovieController::class)->group(function () {
+		Route::get('admin/movies/create', 'create')->name('movies.create');
+		Route::get('admin/movies', 'show')->name('movies.show');
+		Route::post('admin/movie', 'store')->name('movie.store');
+		Route::get('admin/movies/{movie:slug}/edit', 'edit')->name('movie.edit');
+		Route::patch('admin/movies/{movie:slug}', 'update')->name('movie.update');
+		Route::delete('admin/movies/{movie:slug}', 'destroy')->name('movie.destroy');
+	});
+});
