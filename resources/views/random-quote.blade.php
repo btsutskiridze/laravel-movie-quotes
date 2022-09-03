f2eeb03ea45d8ed510fb6d329cfc8e14732d4e32
btsutskiridze <bakatsutskiridze@redberry.ge>
2022-09-03 19:25:11 +0400

    feat: display random quote

--- resources/views/random-quote.blade.php
+++ resources/views/random-quote.blade.php
@@ -6,14 +6,14 @@
         <p class="text-2xl text-white">{{__('texts.no_quotes_yet')}}!</p>
     @else
     <article id="container" class="w-1/2 text-white">
-        <img src="{{asset('storage/' . $quote->thumbnail)}}" class="mx-auto mt-0 mb-16 rounded-xl" width="550px"  alt="">
-        <p class="text-center text-3xl font-normal mb-20">"{{$quote->title}}"</p>
+        <img src="{{asset('storage/' . $quote->thumbnail)}}" class="mx-auto mt-0 mb-16 rounded-xl w-[500px] h-[300px] object-cover"  alt="">
+        <p class="text-center text-3xl font-normal mb-16">"{{ucwords($quote->title)}}"</p>
         <h1 class="text-center text-3xl font-normal">
             <a 
                 class="underline" 
                 href="{{route('movie.show_quotes',['movie'=> $quote->movie->slug,])}}"
             >
-                {{$quote->movie->title}}
+                {{ucwords($quote->movie->title)}}
             </a>
         </h1>
     </article>
